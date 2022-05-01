0454cd24b041818d0513ee27cf8075b6b4debd5c
antonio.martin <antonio.martin@schibsted.com>
2022-05-02 01:18:59 +0200

    007: setup image size

--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,21 @@ import Card from "../components/card/card.js";
 import styles from "./index.module.scss";
 import Menu from "../components/menu/menu.js";
 
-const baseClass = styles["ech-movies"];
-const heroClass = styles["ech-hero"];
-
 const array = [];
 const movie = (index) => <Card key={index} />;
 
 export default function Home() {
   return (
     <section>
-      <section className={heroClass}></section>
       <Menu></Menu>
-      <section className={"container"}>
+      <section className={styles["ech-hero"]}></section>
+      <section className={styles["ech-container"]}>
         This example adds a property <code>getLayout</code> to your page,
         allowing you to return a React component for the layout. This allows you
         to define the layout on a per-page basis. Since we are returning a
         function, we can have complex nested layouts if desired.
       </section>
-      <div className={baseClass}>
+      <div className={styles["ech-movies"]}>
         {array.map((item, index) => movie(index))}
       </div>
       )
